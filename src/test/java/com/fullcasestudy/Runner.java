b189037115fb5ca4eadf46f363a2c1d1ccf61545
likhitkumar <likhitkumar@192.168.0.4>
2020-04-15 11:40:50 -0400

    j

--- src/test/java/com/fullcasestudy/Runner.java
+++ src/test/java/com/fullcasestudy/Runner.java
@@ -4,10 +4,10 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-//import cucumber.api.junit.Cucumber;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty","json:F:\\selenium\\reports\\cucumber.json"},tags= {"@TestmeApp"})
-public class Runner3 {
+public class Runner {
 
 } 
